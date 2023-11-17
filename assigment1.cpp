d86b756c40c23361c868159e3dcf2d3bf05db026
Fathan Rasi Muhammad <96809040+FathanRasiM@users.noreply.github.com>
2023-11-17 11:50:25 +0800

    Q1Doneassigment1.cpp

--- assigment1.cpp
+++ assigment1.cpp
@@ -1,5 +1,33 @@
 #include <iostream>
 using namespace std;
 
+class Student {
+  private:
+    string studentName;
+    string matrixNumber;
+    float courseworkMark;
+    float finalexamMark;
+  public:
+    Student(string sn, string mn, float cm, float fm) {
+      studentName = sm;
+      matrixNumber = mn;
+      courseworkMark = cm;
+      finalexamMark = fm;
+    }
+    ~Student() {
+      
+    }
+    float getTotalMark() {
+      
+    }
+    string getGrade() {
+    
+    }
+    void printResult() {
+
+    }
+    
+}
+
 int main() {
 }
